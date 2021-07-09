afa3c1e20bc3e2a9f3bd50ecfe1d70825b7ecb36
Nguyen Gia Thinh <nguyengiathinh004@gmail.com>
2021-07-09 22:32:59 +0700

    add login page

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: LayoutBuilder(
@@ -9,7 +8,7 @@ class LoginPage extends StatelessWidget {
       return SingleChildScrollView(
           child: Padding(
         padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-        child: Column(children: [
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Padding(
             padding: const EdgeInsets.only(top: 200.0),
             child: Container(
@@ -20,12 +19,144 @@ class LoginPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(top: 20.0),
             child: Container(
-              child: Text(
-                'Username',
-                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.normal),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Container(
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                        child: TextField(
+                            decoration: InputDecoration(
+                                hintText: 'Username',
+                                icon: Icon(Icons.person),
+                                border: InputBorder.none)),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Container(
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                        child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                hintText: 'Password',
+                                icon: Icon(Icons.lock),
+                                border: InputBorder.none)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30.0),
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                '------- Or -------',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    alignment: Alignment.center,
+                    width: 175.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                              width: 20.0,
+                              height: 20.0,
+                              child: Image(
+                                  image:
+                                      AssetImage('assets/images/google.png'))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Container(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                'Google',
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                        )
+                      ],
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    width: 175.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                              width: 20.0,
+                              height: 20.0,
+                              child: Image(
+                                  image: AssetImage(
+                                      'assets/images/facebook.png'))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Container(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                'Facebook',
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                        )
+                      ],
+                    ))
+              ],
+            ),
+          ),
         ]),
       ));
     }));
